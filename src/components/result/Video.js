b42d9506050524975e946d583e6520407298950e
Sohee KWON <soheekwon@naver.com>
2020-11-22 05:26:37 +0900

    modify result css

--- src/components/result/Video.js
+++ src/components/result/Video.js
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 const Youtube = styled.div`
     z-index: 5;
     position: absolute;
-    top: 19.2rem;
+    left: 1.3rem;
+    top: 5.7rem;
 `;
 function Video ({videoId}) {
     const src = `https://www.youtube.com/embed/${videoId}`
