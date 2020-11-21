4a69e43b13f5c48e3ce334334c9dab4c632d310f
Sohee KWON <soheekwon@naver.com>
2020-11-22 02:05:55 +0900

    modify input and login page

--- src/containers/InputList.js
+++ src/containers/InputList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InputCard from '../components/InputCard';
 import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
 
 const ListWrap = styled.div`
     display: flex;
@@ -10,23 +11,20 @@ const ListWrap = styled.div`
 
 function InputList({ history }) {
     const timeList = [2, 5, 10, 15];
-    const [time, setTime] = React.useState(null);
     
     function inputClick(event) {
         const value = event.target.getAttribute("value");
         console.log(value);
-        setTime(value);
         history.push(`/result/${value}`);
     }
 
     return(
         <ListWrap>
-            <InputCard time={timeList[0]} inputClick={inputClick} />
-            <InputCard time={timeList[1]} inputClick={inputClick} />
-            <InputCard time={timeList[2]} inputClick={inputClick} />
-            <InputCard time={timeList[3]} inputClick={inputClick} />
+            {timeList.map(value => {
+                return <InputCard key={value} time={value} inputClick={inputClick} />
+            })}
         </ListWrap>
     );
 }
 
-export default InputList;
+export default withRouter(InputList);
